5f6a121dceb23d87afc5c4cd0995f526153ab6c6
unknown <Goldberg@AS-NBB-GOLD-40.ad.cornell.edu>
2020-04-08 13:11:08 -0400

    Added discrimination between confirmed and unconfirmed jobs in checkProgress

--- SegmenterServer.py
+++ SegmenterServer.py
@@ -591,7 +591,10 @@ videosAhead=videosAhead
             if self.jobQueue[jobNum]['startTime'] is None:
                 jobsAhead = self.countJobsRemaining(beforeJobNum=jobNum)
                 videosAhead = self.countVideosRemaining(beforeJobNum=jobNum)
-                exitCodePhrase = 'is enqueued, but not started.'
+                if self.jobQueue[jobNum]['confirmed']:
+                    exitCodePhrase = 'is enqueued, but not started.'
+                else:
+                    exitCodePhrase = 'has not been confirmed yet. <form action="/confirmJob/{jobID}"><input class="button button-primary" type="submit" value="Confirm and enqueue job" /></form>'.format(jobID=jobNum)
                 stateDescription = '<br/>There are <strong>{jobsAhead} jobs</strong> \
                                     ahead of you with <strong>{videosAhead} videos</strong> \
                                     remaining. Your job will be enqueued to start as soon \
