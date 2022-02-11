73d42f1c122c8b30940aeedfe85b5f180f52c90f
Brian Kardon <brian.kardon@gmail.com>
2022-02-11 10:56:18 -0500

    Copied network training jupyter notebook, and began modifying for integration with server

---
+++ NetworkTraining.py
@@ -0,0 +1,154 @@
+from keras.models import *
+import numpy as np
+from model import *
+from scipy.io import loadmat
+import json
+import random
+
+# This script uses a set of training data assembled into a .mat file consisting of a stack of images
+#    and a corresponding set of binary masks that label the pixels in the image stacks into two classes.
+#    The script then initializes a randomized U-NET (using the topology defined in the file model.py).
+#    It then initiates training using a given batch size and # of epochs, saving the best net at each
+#    step to the given .hdf5 file path.
+#
+# Written by Teja Bollu, documented and modified by Brian Kardon
+
+def trainNetwork(trained_network_path, training_data_path, augment=True,
+    batch_size=10, epochs=512, image_field_name='imageStack',
+    mask_field_name='maskStack', data_augmentation_parameters={},
+    epoch_progress_callback=None):
+
+    # Data augmentation parameter examples:
+    # rotation_range=None, width_shift_range=0.1,
+    # height_shift_range=0.3, zoom_range=0.4, horizontal_flip=True,
+    # vertical_flip=True
+
+    # augment = boolean flag indicating whether to randomly augment training data
+    # batch_size = Size of training batches (size of batches that dataset is divided into for each epoch):
+    # epochs = Number of training epochs (training runs through whole dataset):
+    # training_data_path = .mat file containing the training data (image data and corresponding manually created mask target output):
+    # image_field_name = Field within .mat file that contains the relevant images:
+    # mask_field_name = Field within .mat file that contains the relevant masks:
+    # trained_network_path = File path to save trained network to:
+    # data_augmentation_parameters = to use for data augmentation:
+    # epoch_progress_callback = a function to call at the end of each epoch,
+    #   which takes a progress argument which will be a dictionary of progress
+    #   indicators
+
+    # Reset whatever buffers or saved state exists...not sure exactly what that consists of.
+    # This may not actually work? Word is you have to restart whole jupyter server to get this to work.
+    keras.clear_session()
+
+    # Load training data
+    print('Loading images and masks...')
+    data = loadmat(training_data_path)
+    img = data[image_field_name]
+    mask = data[mask_field_name]
+
+    # Process image and mask data into the proper format
+    img_shape = img.shape;
+    num_samples = img_shape[0];
+    img_size_x = img_shape[1];
+    img_size_y = img_shape[2];
+    img = img.reshape(num_samples, img_size_x, img_size_y, 1)
+    mask = mask.reshape(num_samples, img_size_x, img_size_y, 1)
+
+    print("...image and mask data loaded.")
+    print("Image stack dimensions:", img.shape)
+    print(" Mask stack dimensions:", mask.shape)
+
+    if augment:
+        imgGen = ImageDataGenerator(**data_augmentation_parameters)
+        maskGen = ImageDataGenerator(**data_augmentation_parameters)
+
+    # Load network structure from model.py file
+    lickbot_net = unet(net_scale = 1)
+
+    # Instruct training algorithm to save best network to disk whenever an improved network is found.
+    model_checkpoint = ModelCheckpoint(trained_network_path, monitor='loss',verbose=1, save_best_only=True)
+    callback_list = [model_checkpoint]
+    if epoch_progress_callback is not None:
+        callback_list.append(epoch_progress_callback)
+
+    if augment:
+        print("Using automatically augmented training data.")
+        # Train network using augmented dataset
+        seed = random.randint(0, 1000000000)
+        imgIterator = imgGen.flow(img, seed=seed, shuffle=False, batch_size=batch_size)
+        maskIterator = maskGen.flow(mask, seed=seed, shuffle=False, batch_size=batch_size)
+
+        steps_per_epoch = int(num_samples / batch_size)
+        lickbot_net.fit(
+            ((imgBatch, maskBatch) for imgBatch, maskBatch in zip(imgIterator, maskIterator)),
+            steps_per_epoch=steps_per_epoch, # # of batches of generated data per epoch
+            epochs=epochs,
+            verbose=1,
+            callbacks=callback_list
+        )
+    else:
+        lickbot_net.fit(
+            img,
+            mask,
+            epochs=epochs,
+            verbose=1,
+            callbacks=callback_list
+        )
+
+class TrainingProgressCallback(keras.callbacks.Callback):
+    # def on_train_begin(self, logs=None):
+    #     keys = list(logs.keys())
+    #     print("Starting training; got log keys: {}".format(keys))
+    #
+    # def on_train_end(self, logs=None):
+    #     keys = list(logs.keys())
+    #     print("Stop training; got log keys: {}".format(keys))
+    #
+    # def on_epoch_begin(self, epoch, logs=None):
+    #     keys = list(logs.keys())
+    #     print("Start epoch {} of training; got log keys: {}".format(epoch, keys))
+    def __init__(self):
+        super(TrainingProgressCallback, self).__init__()
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("End epoch {} of training; got log keys: {}".format(epoch, keys))
+
+def validateNetwork(trained_network_path, imgIterator, maskIterator):
+    # Load trained network
+    lickbot_net = load_model(trained_network_path)
+
+    if augment:
+        img_validate = imgIterator.next()
+        mask_validate = maskIterator.next()
+    else:
+        print('Using original dataset for visualization')
+        img_validate = img
+        mask_validate = mask
+
+    mask_pred = lickbot_net.predict(img_validate)
+    mask_pred.shape
+
+    %matplotlib inline
+    from matplotlib import pyplot as plt
+    from matplotlib import gridspec
+
+    numValidation = img_validate.shape[0]
+
+
+    img_disp =                       img_validate.reshape(numValidation,img_size_x,img_size_y)
+    mask_disp =                     mask_validate.reshape(numValidation,img_size_x,img_size_y)
+    mask_pred = lickbot_net.predict(img_validate).reshape(numValidation,img_size_x,img_size_y)
+
+    scaleFactor = 3
+
+    plt.figure(figsize=(scaleFactor*3,scaleFactor*numValidation))
+    plt.subplots_adjust(wspace=0, hspace=0)
+    gs = gridspec.GridSpec(nrows=numValidation, ncols=3, width_ratios=[1, 1, 1],
+             wspace=0.0, hspace=0.0, bottom=0, top=1, left=0, right=1)
+    for k in range(numValidation):
+        plt.subplot(gs[k, 0])
+        plt.imshow(mask_disp[k])
+        plt.subplot(gs[k, 1])
+        plt.imshow(mask_pred[k])
+        plt.subplot(gs[k, 2])
+        plt.imshow(img_disp[k])
